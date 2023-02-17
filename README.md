d8c452985442153030f97f99e353e9061aca0b7d
AniTei <115176414+AniTei@users.noreply.github.com>
2023-02-17 17:27:41 +0100

    i think i did it??

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Make a call to the following endpoint:
 
 ```
 https://api.noroff.dev/api/v1/old-games/2
+
+// successfully called the endpoint
 ```
 
 Display the name, description and image of the game returned.
--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
 // making a fork
+
+const url = "https://api.noroff.dev/api/v1/old-games/2";
+
+async function getOldGames (){
+    try {
+        const respond = await fetch(url);
+        const data = await respond.json();
+
+        console.log (data);
+
+        const outmostDiv = document.querySelector (".container")
+
+outmostDiv.innerHTML = "";
+
+outmostDiv.innerHTML = `<div> 
+                        <h2> Name of game: ${data.name} </h2> 
+                        <p> About this game:</p> <p>${data.description}</p>
+                        <img src="${data.image}" alt="${data.name}">
+                        
+                    </div>`;
+
+
+
+    }
+
+    catch(error){
+
+        console.log (error, "Ooopsie, finn minst en feil")
+
+    }
+}
+
+getOldGames();
+
+
+
+
+//Display the name, description and image of the game returned.
+// first, display anything
+
